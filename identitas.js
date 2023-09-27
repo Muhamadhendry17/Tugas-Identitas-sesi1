15c308eb07d9f547e9bd23c6a75fc2701faed9f6
hendryawnn <109840559+Muhamadhendry17@users.noreply.github.com>
2023-09-27 12:32:13 +0700

    Add files via upload

---
+++ identitas.js
@@ -0,0 +1,23 @@
+// Deklarasi variabel identitas
+const nama = "Muhamad Hendry Irawan Ramadhan";
+const nim = "20220040080";
+
+// Deklarasi variabel untuk operasi aritmatika
+const angka1 = 50;
+const angka2 = 20;
+
+// Menampilkan identitas
+console.log("Nama: " + nama);
+console.log("NIM: " + nim);
+
+// Melakukan operasi aritmatika
+const hasilPenjumlahan = angka1 + angka2;
+const hasilPengurangan = angka1 - angka2;
+const hasilPerkalian = angka1 * angka2;
+const hasilPembagian = angka1 / angka2;
+
+// Menampilkan hasil operasi aritmatika
+console.log("Hasil Penjumlahan: " + hasilPenjumlahan);
+console.log("Hasil Pengurangan: " + hasilPengurangan);
+console.log("Hasil Perkalian: " + hasilPerkalian);
+console.log("Hasil Pembagian: " + hasilPembagian);
